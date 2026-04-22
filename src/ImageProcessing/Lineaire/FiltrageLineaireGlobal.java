2e6b132a827734c343b675d0609e627d19ddf86e
Theunissen Thibault <t.theunissen@mfasprl.be>
2026-04-22 11:08:13 +0200

    FEATURE | todo

--- src/ImageProcessing/Lineaire/FiltrageLineaireGlobal.java
+++ src/ImageProcessing/Lineaire/FiltrageLineaireGlobal.java
@@ -5,24 +5,36 @@ public class FiltrageLineaireGlobal {
     public static int [][] filtrePasseBasIdeal(int [][] image, int frequenceCoupure) {
 
         System.out.println("Fonction filtrePasseBasIdeal");
+
+        // todo
+
         return null;
     }
 
     public static int [][] filtrePasseHautIdeal(int [][] image, int frequenceCoupure) {
 
         System.out.println("Fonction filtrePasseHautIdeal");
+
+        // todo
+
         return null;
     }
 
     public static int [][] filtrePasseBasButterworth(int [][] image, int frequenceCoupure, int ordre) {
 
         System.out.println("Fonction filtrePasseBasButterworth");
+
+        // todo
+
         return null;
     }
 
     public static int [][] filtrePasseHautButterworth(int [][] image, int frequenceCoupure, int ordre) {
 
         System.out.println("Fonction filtrePasseHautButterworth");
+
+        // todo
+
         return null;
     }
 }
--- src/ImageProcessing/Lineaire/FiltrageLineaireLocal.java
+++ src/ImageProcessing/Lineaire/FiltrageLineaireLocal.java
@@ -5,12 +5,18 @@ public class FiltrageLineaireLocal {
     public static int [][] filtreMasqueConvolution(int [][] image, double [][] masque) {
 
         System.out.println("Fonction filtreMasqueConvolution");
+
+        // todo
+
         return null;
     }
 
     public static int [][] filtreMoyenneur(int [][] image, int tailleMasque) {
 
         System.out.println("Fonction filtreMoyenneur");
+
+        // todo
+
         return null;
     }
 }
